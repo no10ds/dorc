94a5eeb76366941d3143a0a64d333aad4ab000cc
Toby Drane <tdrane@no10.gov.uk>
2023-05-17 12:58:20 +0100

    provider changes

--- utils/provider.py
+++ utils/provider.py
@@ -3,5 +3,8 @@ from pulumi_aws import Provider
 
 def create_aws_provider(region: str):
     return Provider(
-        resource_name="aws_provider", region=region, skip_credentials_validation=True
+        resource_name="aws_provider",
+        region=region,
+        skip_credentials_validation=True,
+        skip_metadata_api_check=False,
     )
